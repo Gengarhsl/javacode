1e78fb7323940671dc64f0a79f0bf0c7a7e7969b
Gengarhsl <851178515@qq.com>
2024-11-18 23:59:30 +0800

    修改bug

--- src/main/java/com/example/controller/tUserController.java
+++ src/main/java/com/example/controller/tUserController.java
@@ -2,7 +2,6 @@ package com.example.controller;
 
 import com.example.entity.tUser;
 import com.example.service.tUserService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
 @RequestMapping("tUser")
 public class tUserController {
 
